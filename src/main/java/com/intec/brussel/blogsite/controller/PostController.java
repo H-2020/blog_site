434eaa5e7bfe53906637a0507115c1cc225d9d60
Rahime Yildiz <rahime1yildiz@gmail.com>
2022-03-10 20:04:14 +0100

    add update page

--- src/main/java/com/intec/brussel/blogsite/controller/PostController.java
+++ src/main/java/com/intec/brussel/blogsite/controller/PostController.java
@@ -30,6 +30,7 @@ public class PostController {
     @PostMapping("/create")
     public String create(@ModelAttribute Post post) {
         this.postService.createPost(post);
+
         return "redirect:/blog-post";
     }
 
